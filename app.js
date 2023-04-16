ae54f6ce52df4b81f59f4e5d43582b7c0787a0bd
vasuGoel17 <goelvasu17@gmail.com>
2023-04-17 02:02:45 +0530

    initial\ commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/router");
